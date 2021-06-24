71fa8e0fe4d2724a9627a130cbebe0c055844326
Olena-Kandibula <bakkara@ukr.net>
2021-06-24 21:34:04 +0300

    task-02 end

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ingredientsEl = document.querySelector("#ingredients");
+ingredientsEl.style.listStyleType = 'none';
+ 
+const items = ingredients.map(options => {
+ const itemEl =  document.createElement("li");
+ itemEl.textContent = options;
+
+ return itemEl;
+});
+
+ingredientsEl.append(...items);
+
+// console.log(...items)
