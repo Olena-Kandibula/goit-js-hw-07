5f1d5d5525e7da709d43f9b861dae61de74b2d52
Olena-Kandibula <bakkara@ukr.net>
2021-07-01 23:07:36 +0300

    hw-07 cmpleted

---
+++ js/task-06.js
@@ -0,0 +1,22 @@
+const input = document.querySelector("#validation-input");
+
+input.addEventListener("blur", onInputBlur);
+
+function onInputBlur(event) {
+     
+
+    if (event.currentTarget.value.length ===
+        Number(input.getAttribute('data-length'))) {
+        
+        input.classList.add("valid")
+       input.classList.replace("invalid","valid")
+       
+        return;
+    }
+
+    input.classList.add("invalid")
+    
+}
+
+
+ 
